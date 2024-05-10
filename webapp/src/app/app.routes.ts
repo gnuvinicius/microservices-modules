ca51919a26a12ec08b7a9a19eeb4a0161985eea7
Vinicius Claudino <gnu.vinicius@gmail.com>
2024-05-10 18:24:37 -0300

    wip

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -4,11 +4,11 @@ import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
   {
-    path: 'auth',
+    path: '/webapp/auth',
     component: LoginComponent
   },
   {
-    path: 'signup',
+    path: '/webapp/signup',
     component: SignupComponent
   }
 ];
