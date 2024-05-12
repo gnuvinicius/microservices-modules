04dee7a7111b6d5c745e6ddeb4313d590e9dbd45
Vinicius Claudino <gnu.vinicius@gmail.com>
2024-05-12 19:24:24 -0300

    wip

--- webapp/run-webapp-container.sh
+++ webapp/run-webapp-container.sh
@@ -4,6 +4,6 @@ docker rm webapp -f
 
 docker build --no-cache --tag webapp .
 
-docker run -d -p 9080:80 --name webapp webapp
+docker run -d -p 9080:80 --name webapp gnuvinicius/webapp:staging
 
 docker logs webapp -f
