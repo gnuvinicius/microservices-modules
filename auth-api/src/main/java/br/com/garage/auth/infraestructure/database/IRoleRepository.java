367280a2fef9dcf2bcdbaf240f4996fd0018cb6f
Vinicius Claudino <gnu.vinicius@gmail.com>
2024-05-19 10:06:37 -0300

    wip

--- auth-api/src/main/java/br/com/garage/auth/infraestructure/database/IRoleRepository.java
+++ auth-api/src/main/java/br/com/garage/auth/infraestructure/database/IRoleRepository.java
@@ -2,9 +2,7 @@ package br.com.garage.auth.infraestructure.database;
 
 import br.com.garage.auth.domains.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
