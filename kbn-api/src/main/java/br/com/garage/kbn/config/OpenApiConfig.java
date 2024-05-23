5b5bfe0d603e916c7d7ab62a6cd10be7d64f7548
Vinicius Claudino <gnu.vinicius@gmail.com>
2024-05-23 19:31:57 -0300

    wip

---
+++ kbn-api/src/main/java/br/com/garage/kbn/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package br.com.garage.kbn.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+info = @Info(description = "OpenApi documentation para o webservice Auth-api",
+		title = "Auth-api - documentação", version = "1.0"),
+servers = {
+		@Server(description = "Developer", url = "http://localhost:8080"),
+		@Server(description = "Staginig", url = "http://192.168.0.237") },
+security = { @SecurityRequirement(name = "bearerAuth") })
+@SecurityScheme(name = "bearerAuth", description = "JWT auth description", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
+public class OpenApiConfig {
+
+}
